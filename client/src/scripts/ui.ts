7e62d0871d5b474792036ce5fc3c7171c88b8e49
NeonPSH Admin <admin@neonpsh.ru>
2025-09-06 13:01:20 +0300

    Remove social media, partners and featured content from main page

--- client/src/scripts/ui.ts
+++ client/src/scripts/ui.ts
@@ -1050,15 +1050,6 @@ export async function setUpUI(): Promise<void> {
 
     $<HTMLButtonElement>("#close-report").on("click", () => ui.reportingModal.fadeOut(250));
 
-    const partnersModal = $("#partners-modal");
-
-    $("#partners-link").on("click", () => {
-        if (partnersModal.is(":visible")) return;
-        $(".dialog").fadeOut(250);
-        partnersModal.fadeToggle(250);
-    });
-
-    $("#close-partners").on("click", () => partnersModal.fadeOut(250));
 
     const role = GameConsole.getBuiltInCVar("dv_role");
 
